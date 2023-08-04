1151497f0588ca1e7c051176f61cc4e07a3bee65
eldar2malkic <88284877+eldar2malkic@users.noreply.github.com>
2023-08-03 19:06:00 -0500

    fourth commit

--- hangman.py
+++ hangman.py
@@ -6,17 +6,26 @@ word_list = ["aardvark", "baboon", "camel"]
 
 import random
 chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+print(f'Pssst, the solution is {chosen_word}.')
 
 display = []
-for letter in range(len(chosen_word)):
+for _ in range(word_length):
     display += "_"
-print(display)
+
 
 guess = input("Guess a letter: ").lower()
 
-for position in range(len(chosen_word)):
-    letter = chosen_word[position]
-    if letter == guess:
-        display[position] = letter
-
-print(display)
+while "_" in display:
+    guess = input("Guess a letter: ").lower()
+    
+    #Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
+    
+    print(display)
+print("You win")
