589f7c9e33efdac412ad15e2964467b8aef1d5ed
eldar2malkic <88284877+eldar2malkic@users.noreply.github.com>
2023-08-03 19:16:59 -0500

    fifth commit

--- hangman.py
+++ hangman.py
@@ -1,31 +1,128 @@
 #Step 1 
 
-word_list = ["aardvark", "baboon", "camel"]
+# word_list = ["aardvark", "baboon", "camel"]
 
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+# #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+
+# import random
+# chosen_word = random.choice(word_list)
+# word_length = len(chosen_word)
+
+# print(f'Pssst, the solution is {chosen_word}.')
+
+# display = []
+# for _ in range(word_length):
+#     display += "_"
+
+
+# guess = input("Guess a letter: ").lower()
+
+# while "_" in display:
+#     guess = input("Guess a letter: ").lower()
+    
+#     #Check guessed letter
+#     for position in range(word_length):
+#         letter = chosen_word[position]
+#         # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+#         if letter == guess:
+#             display[position] = letter
+    
+#     print(display)
+# print("You win")
+
+#Step 4
 
 import random
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+end_of_game = False
+word_list = ["ardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
+lives = 6
 
+#Testing code
 print(f'Pssst, the solution is {chosen_word}.')
 
 display = []
 for _ in range(word_length):
     display += "_"
 
-
-guess = input("Guess a letter: ").lower()
-
-while "_" in display:
+while not end_of_game:
     guess = input("Guess a letter: ").lower()
-    
-    #Check guessed letter
+
     for position in range(word_length):
         letter = chosen_word[position]
         # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
         if letter == guess:
             display[position] = letter
-    
-    print(display)
-print("You win")
+
+    if guess not in chosen_word:
+      lives -= 1
+      if lives == 0:
+        end_of_game = True
+        print("You lose.")
+
+    print(f"{' '.join(display)}")
+
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    print(stages[lives])
