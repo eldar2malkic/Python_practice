679eba622e0e4a2d0d655bdb8903c84db77fdf3a
eldar2malkic <88284877+eldar2malkic@users.noreply.github.com>
2023-08-15 17:32:25 -0500

    aa

--- student_scores_dict.py
+++ student_scores_dict.py
@@ -14,11 +14,11 @@ student_grades = {}
 #TODO-2: Write your code below to add the grades to student_grades.👇
 
 for key in student_scores:
-    if student_scores[key] >=91 and student_scores[key] <= 100:
+    if student_scores[key] > 90:
         student_grades[key] = "Outstanding"
-    elif student_scores[key] >=81 and student_scores[key] <= 90:
+    elif student_scores[key] > 80:
         student_grades[key] = "Exceeds Expectations"
-    elif student_scores[key] >=71 and student_scores[key] <= 80:
+    elif student_scores[key] > 70:
         student_grades[key] = "Acceptable"
     else: 
         student_grades[key] = "Fail"
