c4f4281044778f8c682e6e15f89216472f0ac02e
eldar2malkic <88284877+eldar2malkic@users.noreply.github.com>
2023-08-17 13:35:38 -0500

    black commit

--- days_of_the_month.py
+++ days_of_the_month.py
@@ -12,18 +12,20 @@ def is_leap(year):
 
 def days_in_month(year, month):
     month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    if is_leap(year) and month == 2:
+    if month > 12 or month < 1:
+        return "invalid input"
+    elif is_leap(year) and month == 2:
         return 29
     else:
         return month_days[month - 1]
   
   
-# month_names = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+month_names = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
   
-#🚨 Do NOT change any of the code below 
+
 year = int(input("Enter a year: "))
 month = int(input("Enter a month: "))
 days = days_in_month(year, month)
-# name = month_names[month - 1]
-print(days)
-# print(f"{name} of {year} has {days} days.")
+name = month_names[month - 1]
+# print(days)
+print(f"{name} of {year} has {days} days.")
